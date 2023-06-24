928186c62795d313fcc8614e8d21866b9d402892
AdminTanvir1 <126566151+AdminTanvir1@users.noreply.github.com>
2023-06-24 20:59:30 +0000

    under construction

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello dear, missing Tanvir? <br>He is busy with creating His personal website.</p><br><br><h1>কাঠ গোলাপের সাদার মায়া মিশিয়ে দিয়ে ভাবি <br>আবছা নীল তোমার লাগে ভালো।</h1><br><img src="https://photos.app.goo.gl/vAki3eF77ygQ4U7j8">
+    return "<h1>Hello dear,<br>This website is under construction by Tanvir..</h1>
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
