a198faca1352c32b188ef92f9ea6c428ab2e42b5
AdminTanvir1 <126566151+AdminTanvir1@users.noreply.github.com>
2023-06-28 07:14:25 +0000

    many changes

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello dear,<br>This website is under construction by Tanvir...</h1>"
+    return render_template('index.html')
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
+
+@app.route('/img')
+def serve_static(filename):
+    root_dir = os.path.dirname(os.getcwd())
+    return send_from_directory(os.path.join(root_dir, 'img'), filename)
+  
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
